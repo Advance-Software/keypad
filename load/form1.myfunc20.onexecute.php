aa157344878c6adcf90da30e54aec6d8b7c07439
Advance-Software <karen99wot@inbox.ru>
2015-07-29 22:24:17 +0400

    Create form1.myfunc20.onexecute.php

---
+++ load/form1.myfunc20.onexecute.php
@@ -0,0 +1,11 @@
+<?php global $map;
+if($map == 0){
+c("shape2")->x = 183;
+c("pages2")->pageIndex = 3;
+c("label8")->caption = "  Project";
+c("richEdit1")->hide();
+c("html")->hide();
+c("listBox3")->hide();
+c("Project")->show();
+}
+c("timer5")->enable = true;
