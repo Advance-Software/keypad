889cb27a1d8d2d9c07fbea177d76ed8404711651
Advance-Software <karen99wot@inbox.ru>
2015-07-29 22:29:03 +0400

    Create form1.oncreate.php

---
+++ load/form1.oncreate.php
@@ -0,0 +1,9 @@
+<?php $self->doubleBuffered = true;
+TSynHighlighter($self, array('HTML', 'SynHTML'));
+TSynHighlighter($self, array('PHP', 'SynPHP'));
+TSynHighlighter($self, array('General', 'SynGeneral'));
+TSynHighlighter($self, array('Cpp', 'SynCpp'));
+TSynHighlighter($self, array('Css', 'SynCss'));
+TSynHighlighter($self, array('SQL', 'SynSQL'));
+TSynHighlighter($self, array('JScript', 'SynJScript'));
+TSynHighlighter($self, array('XML', 'SynXML'));
