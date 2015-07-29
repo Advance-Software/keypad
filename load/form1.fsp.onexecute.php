9d936a5a6275a45f12e5afd5dd46a3432ad96bab
Advance-Software <karen99wot@inbox.ru>
2015-07-29 16:52:00 +0400

    Create form1.fsp.onexecute.php

---
+++ load/form1.fsp.onexecute.php
@@ -0,0 +1,16 @@
+<?php 
+if(c("Form1->controltab")->caption == 1){
+$x = c("Form1->pages1")->pageIndex + 1;
+$namet = "memo";
+}
+if(c("Form1->controltab")->caption == 2){
+$x = c("Form1->pages3")->pageIndex + 1;
+$namet = "tmemo";
+}
+c("search->edit1")->text = c("$namet$x")->selText;
+$text = c("Form1->$namet$x")->text;
+$pos = strrpos($text, c("search->edit1")->text);
+if ($pos === false)
+return;
+else
+c("Form1->search1")->enable = true;
