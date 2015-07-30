43ce2e71e8fd80b8a05ef44ee551bf6df7c6fe0c
Advance-Software <karen99wot@inbox.ru>
2015-07-30 16:40:03 +0400

    Create form1.spbutton27.OnClick.php

---
+++ load/form1.spbutton27.OnClick.php
@@ -0,0 +1,10 @@
+<?php global $map;
+if($map == 0){
+c("shape2")->x = 183;
+c("pages2")->pageIndex = 0;
+c("label8")->caption = "  Map";
+c("richEdit1")->show();
+c("html")->hide();
+c("listBox3")->hide();
+}
+c("timer5")->enable = true;
