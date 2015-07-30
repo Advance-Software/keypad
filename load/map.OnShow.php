4404d4fc238d57ea470466ed18d1e4aebb7748b5
Advance-Software <karen99wot@inbox.ru>
2015-07-30 16:57:55 +0400

    Create map.OnShow.php

---
+++ load/map.OnShow.php
@@ -0,0 +1,6 @@
+<?php c("map")->formStyle = fsStayOnTop;
+global $map;
+$map = 1;
+c("timer2")->enable = true;
+c("tree1")->text = c("Form1->tree1")->text;
+c("tree1")->absIndex = c("Form1->tree1")->absIndex;
