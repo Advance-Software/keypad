235aa11a8415d7d5201baf4a9e82b89255cad594
Advance-Software <karen99wot@inbox.ru>
2015-07-30 17:03:09 +0400

    Create search.spbutton3.OnMouseUp.php

---
+++ load/search.spbutton3.OnMouseUp.php
@@ -0,0 +1,14 @@
+<?php if(c("Form1->controltab")->caption == 1){
+$x = c("Form1->pages1")->pageIndex + 1;
+$namet = "memo";
+}
+if(c("Form1->controltab")->caption == 2){
+$x = c("Form1->pages3")->pageIndex + 1;
+$namet = "tmemo";
+}
+$text = c("Form1->$namet$x")->text;
+$pos = strrpos($text, c("edit1")->text);
+if ($pos === false)
+return;
+else
+c("Form1->search3")->enable = true;
