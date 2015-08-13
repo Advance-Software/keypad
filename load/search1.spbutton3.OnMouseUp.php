0bbc5bf6cfd91373bcfcd4fb03bc3b9226fd4a5f
Advance-Software <karen99wot@inbox.ru>
2015-08-13 16:30:59 +0400

    Create search1.spbutton3.OnMouseUp.php

---
+++ load/search1.spbutton3.OnMouseUp.php
@@ -0,0 +1,57 @@
+<?php if(c("search->checkbox1")->checked == true){
+if(c("Form1->controltab")->caption == 1){
+$x = c("Form1->pages1")->pageIndex + 1;
+$namet = "memo";
+}
+if(c("Form1->controltab")->caption == 2){
+$x = c("Form1->pages3")->pageIndex + 1;
+$namet = "tmemo";
+}
+$text = c("Form1->$namet$x")->text;
+$pos = strrpos($text, c("edit1")->text);
+if ($pos === false)
+return;
+else
+c("Form1->search7")->enable = true;
+}
+else{
+global $pretext;
+c("search->Search_text")->caption = c("edit1")->text;
+if(c("Form1->controltab")->caption == 1){
+$x = c("Form1->pages1")->pageIndex + 1;
+$namet = "memo";
+}
+if(c("Form1->controltab")->caption == 2){
+$x = c("Form1->pages3")->pageIndex + 1;
+$namet = "tmemo";
+}
+$pretext = c("Form1->$namet$x")->text;
+loadsearch();
+//loading...
+$data = c("search")->caption;
+c("search")->caption = $pretext;
+c("search")->caption = $data;
+//-----------------------------------
+if($pretext != c("Form1->pmemo2")->text){
+c("Form1->pmemo2")->text = c("Form1->$namet$x")->text;
+startsearch();
+}
+if(c("Form1->controltab")->caption == 1){
+$x = c("Form1->pages1")->pageIndex + 1;
+$namet = "memo";
+}
+if(c("Form1->controltab")->caption == 2){
+$x = c("Form1->pages3")->pageIndex + 1;
+$namet = "tmemo";
+}
+$text = c("Form1->$namet$x")->text;
+$pos = strrpos($text, c("search->Search_text")->caption);
+if ($pos === false)
+return;
+else
+c("Form1->search33")->enable = true;
+}
+if(c("search->checkbox3")->checked == true)
+c("search1")->hide();
+else
+return;
