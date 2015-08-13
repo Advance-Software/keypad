1ad63da85c453ecc466d50bb1a9c55e8367d7955
Advance-Software <karen99wot@inbox.ru>
2015-08-13 16:26:46 +0400

    Update form1.myfunc29.onexecute.php

--- load/form1.myfunc29.onexecute.php
+++ load/form1.myfunc29.onexecute.php
@@ -1,5 +1,23 @@
 <?php if(c("Form1")->caption[0] == "*"){
-if(c("num")->items->count == 1 and c("shape3")->x == c("Form1")->w - 17){
+if(c("num")->items->count == 1 and c("shape3")->x != c("image35")->w 
+and c("controltab")->caption == 1){
+$result = messageBox("Save changes?", "Close", MB_YESNOCANCEL);
+ if( $result == idCancel ){
+
+        return false;
+        }
+        if ( $result == idYes ){
+
+        myFunc6();
+        myFunc21();
+           return false;
+        }
+        if( $result == idNo ){
+myFunc21();
+return false;
+}
+}
+if(c("num")->items->count == 1 and c("shape3")->x == c("image35")->w){
 $result = messageBox("Save changes?", "Close", MB_YESNOCANCEL);
 
    if( $result == idCancel ){
@@ -42,7 +60,7 @@ return false;
 }
 }
 else{
-if(c("num")->items->count == 1 and c("shape3")->x == c("Form1")->w - 17){
+if(c("num")->items->count == 1 and c("shape3")->x == c("image35")->w){
 global $upd;
 if($upd == 1)
 run("C:/Users/".$_ENV['USERNAME']."/Desktop/Setup_KeyPad_En.exe");
