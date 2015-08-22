7c1eb73d94b9e10803279804991bd050481a8b6c
Advance-Software <karen99wot@inbox.ru>
2015-08-22 21:57:06 +0400

    Update map.html.OnMouseUp.php

--- load/map.html.OnMouseUp.php
+++ load/map.html.OnMouseUp.php
@@ -1,12 +1,8 @@
 <?php if(c("Form1->controltab")->caption == 1){
 $x = c("Form1->pages1")->pageIndex + 1;
-if(c("html")->itemindex == 0 or c("html")->itemindex == 1 or 
-c("html")->itemindex == 2 or c("html")->itemindex == 3)
-c("Form1->memo$x")->selText = 
-"<".c("html")->items->selected."></".c("html")->items->selected.">";
-if(c("html")->itemindex == 4 or c("html")->itemindex == 5 
-or c("html")->itemindex == 6 or c("html")->itemindex == 7 
-or c("html")->itemindex == 8 or c("html")->itemindex == 9){
+if(c("html")->itemindex == 0 or c("html")->itemindex == 1 or c("html")->itemindex == 2 or c("html")->itemindex == 3)
+c("Form1->memo$x")->selText = "<".c("html")->items->selected."></".c("html")->items->selected.">";
+if(c("html")->itemindex == 4 or c("html")->itemindex == 5 or c("html")->itemindex == 6 or c("html")->itemindex == 7 or c("html")->itemindex == 8 or c("html")->itemindex == 9){
 $data = c("html")->itemIndex - 3;
 c("Form1->memo$x")->selText = "<H$data></H$data>";
 }
@@ -32,17 +28,33 @@ $color = c("Form1->colorDlg1")->color;
 $color = toHTMLColor($color);
 c("Form1->memo$x")->selText = "$color";
 }
+if(c("html")->itemindex == 19)
+c("Form1->memo$x")->selText = "<li></li>";
+if(c("html")->itemindex == 20)
+c("Form1->memo$x")->selText = "<ul></ul>";
+if(c("html")->itemindex == 21)
+c("Form1->memo$x")->selText = "<hr></hr>";
+if(c("html")->itemindex == 22)
+c("Form1->memo$x")->selText = "<table></table>";
+if(c("html")->itemindex == 23)
+c("Form1->memo$x")->selText = "<tr></tr>";
+if(c("html")->itemindex == 24)
+c("Form1->memo$x")->selText = "<td></td>";
+if(c("html")->itemindex == 25)
+c("Form1->memo$x")->selText = "<th></th>";
+if(c("html")->itemindex == 26)
+c("Form1->memo$x")->selText = "<caption></caption>";
+if(c("html")->itemindex == 27)
+c("Form1->memo$x")->selText = "<form></form>";
+if(c("html")->itemindex == 28)
+c("Form1->memo$x")->selText = "<option></option>";
 c("Form1->memo$x")->setFocus();
 }
 if(c("Form1->controltab")->caption == 2){
 $x = c("Form1->pages3")->pageIndex + 1;
-if(c("html")->itemindex == 0 or c("html")->itemindex == 1 
-or c("html")->itemindex == 2 or c("html")->itemindex == 3)
-c("Form1->tmemo$x")->selText = 
-"<".c("html")->items->selected."></".c("html")->items->selected.">";
-if(c("html")->itemindex == 4 or c("html")->itemindex == 5 
-or c("html")->itemindex == 6 or c("html")->itemindex == 7 
-or c("html")->itemindex == 8 or c("html")->itemindex == 9){
+if(c("html")->itemindex == 0 or c("html")->itemindex == 1 or c("html")->itemindex == 2 or c("html")->itemindex == 3)
+c("Form1->tmemo$x")->selText = "<".c("html")->items->selected."></".c("html")->items->selected.">";
+if(c("html")->itemindex == 4 or c("html")->itemindex == 5 or c("html")->itemindex == 6 or c("html")->itemindex == 7 or c("html")->itemindex == 8 or c("html")->itemindex == 9){
 $data = c("html")->itemIndex - 3;
 c("Form1->tmemo$x")->selText = "<H$data></H$data>";
 }
@@ -68,5 +80,25 @@ $color = c("Form1->colorDlg1")->color;
 $color = toHTMLColor($color);
 c("Form1->tmemo$x")->selText = "$color";
 }
+if(c("html")->itemindex == 19)
+c("Form1->tmemo$x")->selText = "<li></li>";
+if(c("html")->itemindex == 20)
+c("Form1->tmemo$x")->selText = "<ul></ul>";
+if(c("html")->itemindex == 21)
+c("Form1->tmemo$x")->selText = "<hr></hr>";
+if(c("html")->itemindex == 22)
+c("Form1->tmemo$x")->selText = "<table></table>";
+if(c("html")->itemindex == 23)
+c("Form1->tmemo$x")->selText = "<tr></tr>";
+if(c("html")->itemindex == 24)
+c("Form1->tmemo$x")->selText = "<td></td>";
+if(c("html")->itemindex == 25)
+c("Form1->tmemo$x")->selText = "<th></th>";
+if(c("html")->itemindex == 26)
+c("Form1->tmemo$x")->selText = "<caption></caption>";
+if(c("html")->itemindex == 27)
+c("Form1->tmemo$x")->selText = "<form></form>";
+if(c("html")->itemindex == 28)
+c("Form1->tmemo$x")->selText = "<option></option>";
 c("Form1->tmemo$x")->setFocus();
 }
