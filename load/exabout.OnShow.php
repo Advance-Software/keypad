89613487b5453038941295696da27936b8297078
Advance-Software <karen99wot@inbox.ru>
2015-08-22 21:54:47 +0400

    Create exabout.OnShow.php

---
+++ load/exabout.OnShow.php
@@ -0,0 +1,7 @@
+<?php global $plagname,$about1,$about2,$about,$copyright;
+c("label1")->caption = $plagname;
+c("label2")->caption = $about1;
+c("label3")->caption = $about2;
+c("label5")->caption = $copyright;
+c("label5")->caption = $version;
+c("Exabout")->formStyle = fsStayOnTop;
